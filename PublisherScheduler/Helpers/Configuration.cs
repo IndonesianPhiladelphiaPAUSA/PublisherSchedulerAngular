57253f5e30770938baeaa90241e854e20ae7b356
Peter Subianto <psubianto@gmail.com>
2020-11-14 00:13:10 -0500

    Added Configuration helper

---
+++ PublisherScheduler/Helpers/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublisherScheduler.Helpers
+{
+    public static class Configuration
+    {
+        public static IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddUserSecrets<Startup>()
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
