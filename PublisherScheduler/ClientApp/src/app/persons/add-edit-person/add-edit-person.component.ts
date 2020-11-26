66c536721d87bac92e2f3942b297f877952d9604
Peter Subianto <psubianto@gmail.com>
2020-11-26 00:07:24 -0500

    Add show-add-edit person component

--- PublisherScheduler/ClientApp/src/app/persons/add-edit-person/add-edit-person.component.ts
+++ PublisherScheduler/ClientApp/src/app/persons/add-edit-person/add-edit-person.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-person',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditPersonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
 
-  ngOnInit() {
+  @Input() person:any;
+  Id:string;
+  Name:string;
+  IsActive:boolean;
+  SecurityLevel:number;
+  PhotoFileName:string;
+  PhotoFilePath:string;
+
+  CapacitiesList:any=[];
+
+  ngOnInit():void {
+    this.Id = this.person.id;
+    this.Name = this.person.name;
+  }
+
+  loadCapacitiesList(){
+    this.service.getAllCapacityNames().subscribe((data:any) => {
+      this.CapacitiesList=data;
+
+      this.Id = this.person.Id;
+      this.Name = this.person.Name;
+      this.IsActive = this.person.IsActive;
+      this.SecurityLevel = this.person.SecurityLevel;
+      this.PhotoFileName = "";
+      this.PhotoFilePath = this.service.PhotoUrl+this.PhotoFileName;
+
+    })
+  }
+
+  addPerson(){
+    var val = { Id:this.Id, Name:this.Name, IsActive:this.IsActive, SecurityLevel:this.SecurityLevel};
+    this.service.addPerson(val).subscribe(data =>
+      alert("Added Successfully!")
+      )
+  }
+
+  updatePerson() {
+    var val = { Id:this.Id, Name:this.Name, IsActive:this.IsActive, SecurityLevel:this.SecurityLevel };
+    this.service.updatePerson(val).subscribe(data =>
+      alert("Updated!")
+      )
   }
 
 }
--- PublisherScheduler/ClientApp/src/app/persons/show-person/show-person.component.ts
+++ PublisherScheduler/ClientApp/src/app/persons/show-person/show-person.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-person',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowPersonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
 
-  ngOnInit() {
+  PersonsList:any=[];
+  ModalTitle:string;
+  ActivateAddEditPersonComp:boolean=false;
+  person:any;
+
+  ngOnInit(): void {
+    this.refreshPersonsList();
+  }
+
+  addClick(){
+    this.person={
+      Id:0,
+      Name:"",
+      IsActive:1,
+      SecurityLevel:0,
+      PhotoFileName:"anonymous.png"
+    }
+    this.ModalTitle="Add Person";
+    this.ActivateAddEditPersonComp=true;
+  }
+
+  editClick(item) {
+    this.person=item;
+    this.ModalTitle="Edit Person";
+    this.ActivateAddEditPersonComp=true;
+  }
+
+  deleteClick(item) {
+    if(confirm("Are you sure?")) {
+      this.service.deletePerson(item.id).subscribe(data => {
+        this.refreshPersonsList();
+      })
+    };
+  }
+
+  closeClick(){
+    this.ActivateAddEditPersonComp=false;
+    this.refreshPersonsList();
+  }
+
+  refreshPersonsList(){
+    this.service.getPersonList().subscribe(data => {
+      console.log(data);
+      this.PersonsList=data;
+    })
   }
 
 }
